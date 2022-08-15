bf28949f2a40da727e6607947044d6fbef8f74e3
nigel-tay <ninenigel@gmail.com>
2022-08-15 17:37:14 +0800

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
     #parentheses are optional in Ruby methods. In this case validates(:name, presence: true) is the same thing
     #if hashes are the last argument in a function call, the curly braces are optional
@@ -15,5 +16,25 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    #Returns a random token
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    #forgets a user
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 end
