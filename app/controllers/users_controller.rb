a7d0282e8a5bef95a38cd6b04e5be873c626c6bb
nigel-tay <ninenigel@gmail.com>
2022-08-23 15:38:41 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated?
   end
 
@@ -42,14 +43,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
@@ -61,12 +54,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+   private
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+      # Before filters
+
+      # Confirms the correct user.
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+      end
+
+      def admin_user
+        redirect_to(root_url) unless current_user.admin?
+      end
 end
